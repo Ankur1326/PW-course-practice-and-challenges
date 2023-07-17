8ca1845b4367144e852069278ec4785c962981cd
Ankur1326 <swamiankur46@gmail.com>
2023-07-17 08:29:38 +0530

    update server.js

--- ApiCalls_masterClass/server/server.js
+++ ApiCalls_masterClass/server/server.js
@@ -1,6 +1,6 @@
 const app = require("./app.js")
-// const port = 20000
-const port = process.env.PORT || 20000
+const port = 20000
+// const port = process.env.PORT || 20000
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
