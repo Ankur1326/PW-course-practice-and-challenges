e643735aa3349c6bd2ccb25c4ac9bb5173f25d4a
Ankur1326 <swamiankur46@gmail.com>
2023-07-20 19:13:57 +0530

    add routers file in crud dir

---
+++ crud_live_class/routes/userRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express")
+
+const {home, createUser} = require("../controllers/userController.js")
+
+const router = express.Router()
+
+router.get("/", home)
+router.post('/createuser', createUser)
+
+module.exports = router
