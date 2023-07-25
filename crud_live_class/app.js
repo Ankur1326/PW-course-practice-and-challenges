b3b5f88e561c4a0cc47f720e19fb099b53efa713
Ankur1326 <swamiankur46@gmail.com>
2023-07-25 16:00:39 +0530

    app.js

--- crud_live_class/app.js
+++ crud_live_class/app.js
@@ -1,21 +1,20 @@
 require('dotenv').config();
 const express = require('express')
 const cors = require("cors") // Cross origin request
-const connectToDB = require(`./config/db.js`)
+const connectToDB = require(`./config/db.js`);
+const usermodels = require('./models/usermodels.js');
 
 const app = express();
 // Express middleware
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-
 // init connection to database
 connectToDB();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
-app.get('')
+const userRoutes = require('./routes/userRoutes.js')
+app.use('/', userRoutes)
+
 
 // export default app;
 module.exports = app;  
